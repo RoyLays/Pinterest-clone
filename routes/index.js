bde85b61bfc8c10f5e26436e9f01cc7fa893598c
Sayan Roy <sayanroyedhome45roy@gmail.com>
2024-08-14 01:02:03 +0530

    first commit

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+  res.render('index');
+});
+
+router.get('/register', function(req, res, next) {
+  res.render('register');
+});
+
+module.exports = router;
